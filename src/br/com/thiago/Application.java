a572b9eeff0ee3904b4d9e4166433acf6430a5d6
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-06-15 17:04:41 -0300

    First class position

--- src/br/com/thiago/Application.java
+++
@@ -1,8 +0,0 @@
-package br.com.thiago;
-
-public class Application {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/br/com/thiago/application/Application.java
@@ -0,0 +1,11 @@
+package br.com.thiago.application;
+
+import br.com.thiago.boardgame.Position;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
