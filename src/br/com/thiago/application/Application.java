bc0e7fa57700e1f58931331be7a3bdb2f9e9d2d2
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-06-15 17:13:01 -0300

    Start to implement Board and Piece

--- src/br/com/thiago/application/Application.java
+++ src/br/com/thiago/application/Application.java
@@ -1,11 +1,11 @@
 package br.com.thiago.application;
 
+import br.com.thiago.boardgame.Board;
 import br.com.thiago.boardgame.Position;
 
 public class Application {
 
     public static void main(String[] args) {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
