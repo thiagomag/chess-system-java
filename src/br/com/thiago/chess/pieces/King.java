769668657f26e99b40abb7870b98ce49014cdd63
Thiago de Oliveira Magdalena <thiagomagdalena@gmail.com>
2021-06-15 17:39:09 -0300

    Placing pieces on the board

---
+++ src/br/com/thiago/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package br.com.thiago.chess.pieces;
+
+import br.com.thiago.boardgame.Board;
+import br.com.thiago.chess.ChessPiece;
+import br.com.thiago.chess.Color;
+
+public class King extends ChessPiece {
+
+    public King(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
